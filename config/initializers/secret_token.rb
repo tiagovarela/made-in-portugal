4e23ead489e7e442e59c4f927e891ede8a58fb5d
Tiago Varela <vogait@gmail.com>
2013-06-01 00:30:59 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MadeInPortugal::Application.config.secret_token = '95db9a2dcbb58dc8d8cd40868b1d7994d334e2ece7bf4a246482c9b7dc06725351815a5f29b68af65ff9a9de9e37f27b55ea30ef8efbd429f5f06eb2347053da'
