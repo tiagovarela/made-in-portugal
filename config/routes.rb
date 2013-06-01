cf25bb49b85a48909dbc86b45c07daaaf8b59c49
Tiago Varela <vogait@gmail.com>
2013-06-01 05:59:03 +0100

    Removed conflict remainigs

--- config/routes.rb
+++ config/routes.rb
@@ -52,14 +52,7 @@ MadeInPortugal::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-<<<<<<< HEAD
-=======
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
   
-
->>>>>>> 7f1d35109b2604e28fdd8655f8ebf612755355f5
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
